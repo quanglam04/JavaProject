eead693b0415d5701dfdecaaa17a265d6bceb0e8
quanglam04 <trinhquanglam2k4@gmail.com>
2024-09-21 22:24:40 +0700

    first Project

---
+++ food-store/src/main/java/com/example/food_store/FoodStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.food_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodStoreApplication.class, args);
+	}
+
+}
