554a2554995cca0cada46ecb7547b61944e5e868
quanglam04 <trinhquanglam2k4@gmail.com>
2024-09-24 00:00:49 +0700

    Update 09/24/2024 12:00PMclear

--- food-store/src/main/java/com/example/food_store/FoodStoreApplication.java
+++ food-store/src/main/java/com/example/food_store/FoodStoreApplication.java
@@ -3,7 +3,7 @@ package com.example.food_store;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class FoodStoreApplication {
 
 	public static void main(String[] args) {
