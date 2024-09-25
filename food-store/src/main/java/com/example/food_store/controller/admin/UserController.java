da88f2017f467ca18877abb814564043c1fb958e
quanglam04 <trinhquanglam2k4@gmail.com>
2024-09-25 22:55:11 +0700

    Update createUser

---
+++ food-store/src/main/java/com/example/food_store/controller/admin/UserController.java
@@ -0,0 +1,35 @@
+package com.example.food_store.controller.admin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.food_store.domain.User;
+import com.example.food_store.service.UserService;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @RequestMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("newUser", new User());
+        return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUser(Model model, @ModelAttribute("newUser") User trinhlam) {
+        System.out.println("thong tin" + trinhlam);
+        return "hello";
+    }
+
+}
