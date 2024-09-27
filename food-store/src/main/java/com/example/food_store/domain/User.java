0a9196c9a844915186c700cdba85b2bd514cf1b8
quanglam04 <trinhquanglam2k4@gmail.com>
2024-09-27 23:06:30 +0700

    Update relationships for model 09/27/2024

--- food-store/src/main/java/com/example/food_store/domain/User.java
+++ food-store/src/main/java/com/example/food_store/domain/User.java
@@ -1,20 +1,41 @@
 package com.example.food_store.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
+
     private String password;
     private String fullName;
     private String address;
     private String phone;
+
+    private String avatar;
+
+    // roleId/
+    // User many -> to one -> role
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    List<Order> orders;
 
     public long getId() {
         return id;
@@ -64,10 +85,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
